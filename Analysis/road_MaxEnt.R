7adf2853dc4bcc01bfc962bbe50385fc985110f8
Almapian <alfatihiabdulmalik@gmail.com>
2026-01-08 11:50:33 +0000

    update: added set.seed to road_MaxEnt and saved all output images

--- Analysis/road_MaxEnt.R
+++ Analysis/road_MaxEnt.R
@@ -1,8 +1,18 @@
+# As of last run:
+# AUC: 0.934055555555556
+# best AIC: 5234.56944484783
+# Best model param RegMult: 0.1 Features: t
+
+
+
 library(sf)
+library(tmap)
+library(tools)
 library(terra)
 library(maxnet)
-library(readr)
-library(tidyr)
+library(tidyverse)
+source('utils/boundaries.R')
+source('utils/model_performance.R')
 
 # Define the paths for the two specific files
 selected_tifs <- c("./Data/Tif/employment_accessibility_england.tif",
@@ -10,7 +20,6 @@ selected_tifs <- c("./Data/Tif/employment_accessibility_england.tif",
 
 presence <- rast(selected_tifs)
 
-library(tools)
 names(presence) <- file_path_sans_ext(basename(selected_tifs))
 
 data_centers_sf <- read_csv("Data/Example/UK_Data_Centers.csv") %>%
@@ -25,69 +34,58 @@ presence_vals <- terra::extract(presence, data_centers_sf, ID = FALSE)
 valid_rows <- complete.cases(presence_vals)
 presence_clean <- presence_vals[valid_rows, , drop = FALSE]
 
+set.seed(123)
 bg_data <- spatSample(presence, size = 1000, method = "random", na.rm = TRUE, values = TRUE)
 model_data <- as.data.frame(rbind(presence_clean, bg_data))
 
 # The background point set to 0 because in entropy, presence points are 1, 0 represent random distributed points
-set.seed(123)
 pa <- c(rep(1, nrow(presence_clean)), rep(0, nrow(bg_data)))
 # There is a bug in model, if the input only contain 1 variable, it will cause error bacause  [drop = FALSE]
 me_model <- maxnet(p = pa, data = model_data)
 suitability_map <- predict(presence, me_model, type = "logistic", na.rm = TRUE)
 plot(suitability_map)
 
-# This is response curve
-plot(me_model, type = "logistic")
+# This is to find the best model params using grid search,
+regmult_vals <- c(0.1, 0.5, 1)
+feature_classes <- c("l", "q", "h", "t", "lq")
+grid_search_result <- grid_search(
+  data = model_data,
+  pa = pa,
+  regmult_vals = regmult_vals,
+  feature_classes = feature_classes
+)
 
+me_model <- grid_search_result$best_model
+
+message(paste("Best AIC:", grid_search_result$best_score))
+message(paste("Best model param RegMult:", grid_search_result$best_params[1],
+              "Features:", grid_search_result$best_params[2]))
+
+# This is suitability map
 png("Data/SuitibilityMap/road_data_center_suitability.png", width = 2000, height = 2000, res = 300)
 plot(suitability_map, main = "Data Center Suitability by Connectivity Criteria")
 dev.off()
 
-# AUC
-library(ROCR)
-pred <- predict(me_model, newdata = model_data, type = "logistic")
-pred_obj <- prediction(pred, pa)
-auc <- performance(pred_obj, measure = "auc")
-auc@y.values[[1]]
+# This is response curve
+png("Data/SuitibilityMap/road_model_importance.png", width = 2000, height = 2000, res = 300)
+plot(me_model, type = "logistic")
+dev.off()
 
-pred_obj <- prediction(pred, pa)
-roc_perf <- performance(pred_obj, measure = "tpr", x.measure = "fpr")
+response.plot(me_model, names(model_data)[1], type = "logistic")
 
-# Y (TPR, True Positive Rate / Sensitivity)
-# X (FPR, False Positive Rate)
-plot(roc_perf,
-     main = "ROC Curve",
-     ylab = "True Positive Rate (Sensitivity)",
-     xlab = "False Positive Rate")
-abline(a = 0, b = 1, lty = 2, col = "gray")
+report <- maxent_model_report(me_model, model_data, pa_vector = pa)
 
-# confusion matrix
-library(caret)
-pred_factor <- factor(ifelse(pred > 0.5, 1, 0), levels = c(0, 1))
-actual_factor <- factor(pa, levels = c(0, 1))
+tmap_mode("plot")
+suitability_map <- predict(presence, me_model, type = "logistic", na.rm = TRUE)
+threshold <- report$youden$threshold
+suitability_map[suitability_map <= threshold] <- NA
+smap <- tm_basemap("CartoDB.Positron") +
+  tm_shape(suitability_map) +
+  tm_raster(title = "Suitability", palette = "viridis", alpha = 0.7)+
+  tm_shape(england_bng) +
+  tm_borders(col = "black", alpha = 0.3, lwd = 0.2)
 
-# No Information Rate: if we always predict the majority class
-# P-Value [Acc > NIR]: if our model is significantly better than NIR
-# Kappa: after deducting the probability of "just guessing correctly", the model's true discrimination ability
-# Mcnemar's Test P-Value: Bias, [(true false) - (false true)]^2/[(true false) + (false true)], finding two biased predictions
-# Sensitivity: TP / (TP + FN)
-# Specificity: TN / (TN + FP)
-# Pos Pred Value: TP / (TP + FP)
-# Neg Pred Value: TN / (TN + FN)
-# Prevalence: (TP + FN) / Total (same as NIR)
-# Detection Rate: TP / Total
-# Detection Prevalence: (TP + FP) / Total
-# Balanced Accuracy: (Sensitivity + Specificity) / 2
-confusionMatrix(pred_factor, actual_factor)
-# In our case, false positive means that we predict data centre is a good location when it is not.
-
-cm <- confusionMatrix(pred_factor, actual_factor)
-cm_table <- as.data.frame(cm$table)
-library(tidyverse)
-cm_table%>%
-  ggplot(aes(Reference, Prediction)) +
-  geom_tile(aes(fill = Freq), color = "white") +
-  scale_fill_gradient(low = "white", high = "steelblue") +
-  geom_text(aes(label = Freq), vjust = 1) +
-  labs(title = "Confusion Matrix", fill = "Count") +
-  theme_minimal()
+tmap_save(smap,
+          filename = paste0("Data/SuitibilityMap/road_data_center_suitability_", threshold, ".png"),
+          width = 10, height = 8, units = "in", dpi = 300,
+          device = ragg::agg_png)
