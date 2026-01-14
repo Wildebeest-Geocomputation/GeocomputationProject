7d15239cb98d222e3817ff85cc923037c8645255
Almapian <alfatihiabdulmalik@gmail.com>
2026-01-14 17:48:08 +0000

    update EDA graphs and added l-inhom calculation

---
+++ Analysis/Inhom_l_func.R
@@ -0,0 +1,26 @@
+library(spatstat)
+library(sf)
+library(readr)
+source('utils/boundaries.R')
+
+data_centers_sf <- read_csv("~/GeocomputationProject/Data/Example/UK_Data_Centers.csv") %>%
+  st_as_sf(coords = c("lon", "lat"), crs = 4326) %>%
+  st_transform(27700)
+
+# Convert SpatVector to sf, then union
+england <- st_union(st_as_sf(england_bng))
+england_owin <- as.owin(england)
+
+coords <- st_coordinates(data_centers_sf)
+dc_ppp <- ppp(x = coords[,1],
+              y = coords[,2],
+              window = england_owin)
+
+lambda <- density(dc_ppp, sigma = 100000)
+
+Linhom_result <- Linhom(dc_ppp, lambda = lambda)
+plot(Linhom_result, . - r ~ r, xlim = c(0, 30000))
+abline(h = 0, lty = 2)
+
+L_hom <- Lest(dc_ppp)
+plot(L_hom, . - r ~ r, main="Homogeneous L-function", xlim = c(0, 30000))
